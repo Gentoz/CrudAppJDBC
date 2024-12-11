cca2e0d2bef712edaa74364acf79838033256b91
Sergey Kuznetsov <sk300102@yandex.ru>
2024-12-11 15:22:58 +0300

    init

---
+++ src/main/java/com/github/gentoz/crudappjdbc/CrudAppJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.github.gentoz.crudappjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudAppJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudAppJdbcApplication.class, args);
+    }
+
+}
